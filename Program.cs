63fb45279ef739b6caf130bb79ec3f75986a1007
Aakash Virdhe <aakash.virdhe@gmail.com>
2023-04-03 23:46:15 +0530

    Database Connection and Migration Completed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using eTicket.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// db configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
@@ -26,8 +30,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// db configuration
-builder.Services.AddDbContext<AppDbContext>();
 
 app.Run();
 
